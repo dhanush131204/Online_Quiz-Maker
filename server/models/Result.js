d5adc2a8d3076f9c64b493994e33d3a7eaf1d33f
DHANUSH P <dhanushpdhanushp40@gmail.com>
2025-04-24 19:29:02 +0530

    Update Result.js

--- server/models/Result.js
+++ server/models/Result.js
@@ -1,1 +1,40 @@
+const mongoose = require('mongoose');
 
+const ResultSchema = new mongoose.Schema({
+  quiz: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Quiz',
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true
+  },
+  totalQuestions: {
+    type: Number,
+    required: true
+  },
+  correctAnswers: {
+    type: Number,
+    required: true
+  },
+  answers: [{
+    question: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Quiz.questions'
+    },
+    selectedOption: Number,
+    isCorrect: Boolean
+  }],
+  completedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Result', ResultSchema);
